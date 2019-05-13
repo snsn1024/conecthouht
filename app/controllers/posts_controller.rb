25f3ec66a1a0ad09c6cd14f99194349b4172ff13
snsn1024 <asqqqqqqqqqq@gmail.com>
2019-05-13 19:00:09 +0000

    fix controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
 
-	def create
-		@post = Post.new(post_params)
-		@post.save
-		redirect_to post_path(post.id)
-	end
-
 	def index
 		@post = Post.new
 		@posts = Post.all
+	end
+	
+	def create
+		post = Post.new(post_params)
+		post.save
+		redirect_to post_path(post.id)
 	end
 
 	def show
