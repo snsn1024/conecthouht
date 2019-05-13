263cee21ae5ec11ce6eed0892a8f58b6cf3e825f
snsn1024 <asqqqqqqqqqq@gmail.com>
2019-05-13 16:52:56 +0000

    delete post.new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,17 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
+		@post.save
+		redirect_to post_path(post.id)
 	end
 
 	def index
+		@post = Post.new
 		@posts = Post.all
 	end
 
 	def show
+		@post = Post.find(params[:id]) 
 	end
 
 	private
