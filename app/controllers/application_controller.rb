f63584ab7eab478396be9b7bf5866e0153f49872
snsn1024 <asqqqqqqqqqq@gmail.com>
2019-05-19 07:18:34 +0000

    add search bar[header]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-	
+	before_action :set_search
+
+	def set_search
+		@search = Post.ransack(params[:post_title])
+		@results = @search.result
+	end
 end
